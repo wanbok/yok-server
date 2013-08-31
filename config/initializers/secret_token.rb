4c55f274e551a2449667f845ab172979a3f2fa3f
Wanbok Choi <choi@wanbok.com>
2013-08-31 21:52:12 +0900

    Initial with slim

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Yok::Application.config.secret_key_base = 'ccae9cd9f70e6fe304da9d5ea0809ee1a87ff11b4135e0ee4499b4dc052a9c189f49c104fa2c9bd3450aec1f3769ecf95139d0b7b5a51ac226324526af4e2de0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Yok::Application.config.session_store :cookie_store, key: '_yok_session'
