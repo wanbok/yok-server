c7e53acfb87e39055ebdcf71b1bb93701aa2968a
Wanbok Choi <choi@wanbok.com>
2013-09-01 05:02:10 +0900

    Fix my stupid mistake

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,4 +3,6 @@ class Group < ActiveRecord::Base
   belongs_to :teacher, :class_name => "User"
   has_many :students, through: :teams, class_name: "User", foreign_key: "team_id"
   has_many :teams
-  has_many :missions, :class_name => "Mission", foreign_key: => "group_id"
+  has_many :missions
+  
+end
