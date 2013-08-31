fba63dbc6a5db283aa9e7cf3f72b5ce45869ec87
Wanbok Choi <choi@wanbok.com>
2013-09-01 05:01:19 +0900

    make sure missions relationships

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,4 +3,4 @@ class Group < ActiveRecord::Base
   belongs_to :teacher, :class_name => "User"
   has_many :students, through: :teams, class_name: "User", foreign_key: "team_id"
   has_many :teams
-  has_many :missions
+  has_many :missions, :class_name => "Mission", foreign_key: => "group_id"
