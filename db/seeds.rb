42804fc3c4cbb76cf2d7e4eb46d930a82c1b09a4
Wanbok Choi <choi@wanbok.com>
2013-09-01 10:31:20 +0900

    update seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,14 +9,6 @@
 teacher_kim = User.create({name: '김샘', phonenumber: '01056564321', password: 'password', password_confirmation: 'password', role: :teacher})
 teacher_jeong = User.create({name: '정샘', phonenumber: '01056563321', password: 'password', password_confirmation: 'password', role: :teacher})
 
-student_a = User.create({name: '김학생', phonenumber: '01056569999', password: 'password', password_confirmation: 'password', role: :student})
-student_b = User.create({name: '이학생', phonenumber: '01056569998', password: 'password', password_confirmation: 'password', role: :student})
-student_c = User.create({name: '저학생', phonenumber: '01056569997', password: 'password', password_confirmation: 'password', role: :student})
-student_d = User.create({name: '요학생', phonenumber: '01056569996', password: 'password', password_confirmation: 'password', role: :student})
-student_e = User.create({name: '조학생', phonenumber: '01056569995', password: 'password', password_confirmation: 'password', role: :student})
-student_f = User.create({name: '최학생', phonenumber: '01056569994', password: 'password', password_confirmation: 'password', role: :student})
-
-
 group1 = Group.create({name: '이문초등학교 1학년 1반', teacher: teacher_kim})
 group2 = Group.create({name: '청량초등학교 6학년 13반', teacher: teacher_jeong})
 
@@ -26,6 +18,29 @@ team1_3 = Team.create({name: '호박잎조', group: group1})
 team2_1 = Team.create({name: '깨소금조', group: group2})
 team2_2 = Team.create({name: '하늘조', group: group2})
 team2_3 = Team.create({name: '뭐조', group: group2})
+
+student_a = User.create({name: '김학생', team: team1_1, phonenumber: '01056569999', password: 'password', password_confirmation: 'password', role: :student})
+student_b = User.create({name: '이학생', team: team1_1, phonenumber: '01056569998', password: 'password', password_confirmation: 'password', role: :student})
+student_c = User.create({name: '저학생', team: team1_1, phonenumber: '01056569997', password: 'password', password_confirmation: 'password', role: :student})
+student_d = User.create({name: '요학생', team: team1_1, phonenumber: '01056569996', password: 'password', password_confirmation: 'password', role: :student})
+
+student_e = User.create({name: '조학생', team: team1_2, phonenumber: '01056569995', password: 'password', password_confirmation: 'password', role: :student})
+student_f = User.create({name: '최학생', team: team1_2, phonenumber: '01056569994', password: 'password', password_confirmation: 'password', role: :student})
+student_g = User.create({name: '김철수', team: team1_2, phonenumber: '01045459999', password: 'password', password_confirmation: 'password', role: :student})
+student_h = User.create({name: '이철수', team: team1_2, phonenumber: '01045459998', password: 'password', password_confirmation: 'password', role: :student})
+
+student_i = User.create({name: '저철수', team: team1_3, phonenumber: '01045459997', password: 'password', password_confirmation: 'password', role: :student})
+student_j = User.create({name: '요철수', team: team1_3, phonenumber: '01045459996', password: 'password', password_confirmation: 'password', role: :student})
+student_k = User.create({name: '조철수', team: team1_3, phonenumber: '01045459995', password: 'password', password_confirmation: 'password', role: :student})
+student_l = User.create({name: '최철수', team: team1_3, phonenumber: '01045459994', password: 'password', password_confirmation: 'password', role: :student})
+
+student_m = User.create({name: '김동혁', team: team2_1, phonenumber: '01034349999', password: 'password', password_confirmation: 'password', role: :student})
+student_n = User.create({name: '이동혁', team: team2_1, phonenumber: '01034349998', password: 'password', password_confirmation: 'password', role: :student})
+student_o = User.create({name: '저동혁', team: team2_1, phonenumber: '01034349997', password: 'password', password_confirmation: 'password', role: :student})
+student_p = User.create({name: '요동혁', team: team2_1, phonenumber: '01034349996', password: 'password', password_confirmation: 'password', role: :student})
+
+student_q = User.create({name: '조동혁', team: team2_2, phonenumber: '01034349995', password: 'password', password_confirmation: 'password', role: :student})
+student_r = User.create({name: '최동혁', team: team2_3, phonenumber: '01034349994', password: 'password', password_confirmation: 'password', role: :student})
 
 log1 = Log.create({student: student_a, date: Date.today, curse_count: 10, diary: "허허허"})
 log2 = Log.create({student: student_b, date: Date.today, curse_count: 1, diary: "허허허허"})
